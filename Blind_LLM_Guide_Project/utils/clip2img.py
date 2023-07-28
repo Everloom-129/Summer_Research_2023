7b91eed4a227149090eea6ef17a2e892007beff4
Everloom-129 <jie.20@intl.zju.edu.cn>
2023-07-28 10:47:09 +0800

    enhanced chatbot

--- Blind_LLM_Guide_Project/utils/clip2img.py
+++ Blind_LLM_Guide_Project/utils/clip2img.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 
 # Input video directory
-input_video_dir = "JAAD_clips" # "path_to_your_video_directory"
+input_video_dir = "HZ3" # "path_to_your_video_directory"
 
 # Output directory
 output_dir = ".\\output"
@@ -15,19 +15,17 @@ video_files = [f for f in os.listdir(input_video_dir) if f.endswith('.mp4')]
 for video_file in video_files:
     # Create a VideoCapture object
     cap = cv2.VideoCapture(os.path.join(input_video_dir, video_file))
-
     # Get the frames per second (fps) of the video
     fps = cap.get(cv2.CAP_PROP_FPS)
-
     # Get the total number of frames in the video
     num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
     # Calculate the duration of the video clip
     duration = num_frames / fps
 
-    # Calculate the frame interval for each second
-    frame_interval = math.floor(fps)
-
+    # Calculate the frame interval for each 1/3 second
+    frame_interval = math.floor(fps / 3)
+    if frame_interval == 0:
+        break
     # Create output directory for each video
     video_dir = os.path.join(output_dir, video_file.split(".")[0])
     os.makedirs(video_dir, exist_ok=True)
@@ -45,7 +43,7 @@ for video_file in video_files:
                 seq_num += 1
                 cv2.imwrite(os.path.join(video_dir, f'image_{seq_num:04d}.png'), frame)
 
-            if seq_num == math.floor(duration):  # if already saved images equals to seconds of the video, then break
+            if seq_num == math.floor(duration * 3):  # Adjust the break condition
                 break
         # Break the loop if no frame is returned
         else:
